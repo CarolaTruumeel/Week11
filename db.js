954a2b79530445daa0c42b206f02fd7aaf250001
CarolaTruumeel <123981074+CarolaTruumeel@users.noreply.github.com>
2023-12-12 00:04:04 +0200

    week 9 code

---
+++ db.js
@@ -0,0 +1,11 @@
+const { Pool } = require ("pg");
+
+const itemsPool = new Pool ({
+    user: "postgres",
+    password: "1234",
+    host: "localhost",
+    port: 5432, 
+    database: "myrecipes"
+})
+
+module.exports =  itemsPool;
